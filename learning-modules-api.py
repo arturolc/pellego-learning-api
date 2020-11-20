00069c550f325c4eb439dc24343114a4a516e924
arturolc <arturo.lara@utah.edu>
2020-11-19 17:23:14 -0700

    Setup basics

---
+++ learning-modules-api.py
@@ -0,0 +1,14 @@
+"""
+Arturo Lara-Coronado
+
+Learning Modules API
+"""
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
