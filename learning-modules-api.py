2bb219d84f4e7d8025dab87a595dcbba3430afbc
arturolc <arturo.lara@utah.edu>
2020-11-19 17:28:54 -0700

    Added test resource

--- learning-modules-api.py
+++ learning-modules-api.py
@@ -10,5 +10,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+class LearningModules(Resource):
+    def get(self):
+        return {"test"}
+
+api.add_resource(LearningModules, "/test")
+
 if __name__ == "__main__":
     app.run(debug=True)
