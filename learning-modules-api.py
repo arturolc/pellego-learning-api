8e1ce393837a7c087ab4d4d3e46a948ffe15efa2
Ubuntu <ubuntu@ip-172-31-13-218.us-west-1.compute.internal>
2020-11-30 02:28:31 +0000

    Changed requirements txt, added database connection

--- learning-modules-api.py
+++ learning-modules-api.py
@@ -6,15 +6,21 @@ Learning Modules API
 
 from flask import Flask, request
 from flask_restful import Resource, Api
+import mysql.connector
 
+cnx = mysql.connector.connect(user='admin', password='capstone', host='127.0.0.1', database='pellego_database')
 app = Flask(__name__)
 api = Api(app)
 
 class LearningModules(Resource):
     def get(self):
-        return {"test"}
+        query = "select * from Modules"
+        return {"data":"test"}
 
-api.add_resource(LearningModules, "/test")
+class Quiz(Resource):
+    def get(self, quiz_id):
+        query = "select 
+api.add_resource(LearningModules, "/modules")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
